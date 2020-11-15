a595427510f591a3c473648448f14e8e08b77e0d
secondthunder <awayfromgalaxy@gmail.com>
2020-11-15 16:48:58 +0300

    Fix
    Updated .gitignore
    Changed indents in main.js

--- main.js
+++ main.js
@@ -26,89 +26,90 @@ client.on('message', msg => {
 	const command = args.shift().toLowerCase();
 
 	switch (command) {
-		case 'ху':
-		case 'who':
-			if (msg.author.id === '663283391365644309') {
-				return;
-			}
-			else {
-				client.commands.get('randomCringe').execute(msg);
-			}
-			break;
-		case 'whoiscope':
-			if (!args.length) {
-				client.commands.get('randomGoroscope').execute(msg);
-			}
-			else if (args[0] === 'reset' && msg.author.id !== '663283391365644309') {
-				client.commands.get('goroscopeReset').execute(msg);
-			}
-			break;
-		case 'шип':
-			if (!args.length) {
-				let isCustom = false;
-				client.commands.get('randomShipping').execute(msg, args, isCustom);
-			}
-			else if (args[0] === 'сброс' && msg.author.id !== '663283391365644309') {
-				client.commands.get('shippingReset').execute(msg);
-			} else if (args[0] !== 'сброс') {
-				let isCustom = true;
-				client.commands.get('randomShipping').execute(msg, args, isCustom);
-			}
-			break;
-		case 'add':
-			if (msg.author.id === '663283391365644309') {
-				return;
-			}
-			else if (!args.length) {
-				return msg.channel.send(`${msg.author} чел... введи может что-нибудь`);
-			}
-			else if (args.length) {
-				const textString = args.join(' ');
-				client.commands.get('addWord').execute(msg, textString);
-			}
-			break;
-		case 'delete':
-			if (msg.author.id === '663283391365644309') {
-				return;
-			}
-			else if (!args.length) {
-				return msg.channel.send(`${msg.author} чел... введи может что-нибудь`);
-			}
-			else if (args.length) {
-				const textString = args.join(' ');
-				client.commands.get('deleteWord').execute(msg, textString);
-			}
-			break;
-		case 'рулетка':
-			if (!args.length) {
-				const bulletCount = 1;
-				client.commands.get('russianRoulette').execute(msg, bulletCount);
-			}
-			else if (args.length) {
-				const bulletCount = args[0];
-				client.commands.get('russianRoulette').execute(msg, bulletCount);
-			}
-			break;
-		case 'uptime':
-			client.commands.get('uptime').execute(msg);
-			break;
-		case 'йа':
-			if (!args.length) {
-				return
-			}
-			else {
-				const textString = args.join(' ');
-				client.commands.get('me').execute(msg, textString);
-			}
-			break;
-		case 'хелп':
-			client.commands.get('help').execute(msg);
-			break;
-		case 'exit':
-			client.commands.get('exit').execute(msg);
-			break;
-		default:
-			break;
+	case 'ху':
+	case 'who':
+		if (msg.author.id === '663283391365644309') {
+			return;
+		}
+		else {
+			client.commands.get('randomCringe').execute(msg);
+		}
+		break;
+	case 'whoiscope':
+		if (!args.length) {
+			client.commands.get('randomGoroscope').execute(msg);
+		}
+		else if (args[0] === 'reset' && msg.author.id !== '663283391365644309') {
+			client.commands.get('goroscopeReset').execute(msg);
+		}
+		break;
+	case 'шип':
+		if (!args.length) {
+			const isCustom = false;
+			client.commands.get('randomShipping').execute(msg, args, isCustom);
+		}
+		else if (args[0] === 'сброс' && msg.author.id !== '663283391365644309') {
+			client.commands.get('shippingReset').execute(msg);
+		}
+		else if (args[0] !== 'сброс') {
+			const isCustom = true;
+			client.commands.get('randomShipping').execute(msg, args, isCustom);
+		}
+		break;
+	case 'add':
+		if (msg.author.id === '663283391365644309') {
+			return;
+		}
+		else if (!args.length) {
+			return msg.channel.send(`${msg.author} чел... введи может что-нибудь`);
+		}
+		else if (args.length) {
+			const textString = args.join(' ');
+			client.commands.get('addWord').execute(msg, textString);
+		}
+		break;
+	case 'delete':
+		if (msg.author.id === '663283391365644309') {
+			return;
+		}
+		else if (!args.length) {
+			return msg.channel.send(`${msg.author} чел... введи может что-нибудь`);
+		}
+		else if (args.length) {
+			const textString = args.join(' ');
+			client.commands.get('deleteWord').execute(msg, textString);
+		}
+		break;
+	case 'рулетка':
+		if (!args.length) {
+			const bulletCount = 1;
+			client.commands.get('russianRoulette').execute(msg, bulletCount);
+		}
+		else if (args.length) {
+			const bulletCount = args[0];
+			client.commands.get('russianRoulette').execute(msg, bulletCount);
+		}
+		break;
+	case 'uptime':
+		client.commands.get('uptime').execute(msg);
+		break;
+	case 'йа':
+		if (!args.length) {
+			return;
+		}
+		else {
+			const textString = args.join(' ');
+			client.commands.get('me').execute(msg, textString);
+		}
+		break;
+	case 'хелп':
+		client.commands.get('help').execute(msg);
+		break;
+	case 'exit':
+		client.commands.get('exit').execute(msg);
+		break;
+	default:
+		break;
 	}
 });
 

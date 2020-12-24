34e010b39a17b6559fa1146393a5a46334fd78f3
SecondThundeR <awayfromgalaxy@gmail.com>
2020-12-24 15:40:02 +0300

    Change text for "RSP" game

--- commands/rspGame.js
+++ commands/rspGame.js
@@ -4,31 +4,31 @@ function rspGame(msg, args) {
 	const randomMove = moveVariants[Math.floor(Math.random() * moveVariants.length)];
 	const userMove = args[0];
 	if (randomMove === userMove) {
-		msg.channel.send(`Так как у нас обоих ${randomMove}, то у нас ничья!`);
+		msg.channel.send(`Так как у нас обоих **${randomMove}**, то у нас ничья!`);
 		return;
 	}
 	else if (userMove === 'камень' && randomMove === 'ножницы') {
-		msg.channel.send(`${msg.author}! Ты победил. У меня были ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. Ты победил, потому что у меня были **${randomMove}**`);
 		return;
 	}
 	else if (userMove === 'камень' && randomMove === 'бумага') {
-		msg.channel.send(`${msg.author}! Я победил. У меня была ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. А я вот победил, потому что у меня была **${randomMove}**`);
 		return;
 	}
 	else if (userMove === 'ножницы' && randomMove === 'бумага') {
-		msg.channel.send(`${msg.author}! Ты победил. У меня была ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. Ты победил, потому что у меня была **${randomMove}**`);
 		return;
 	}
 	else if (userMove === 'ножницы' && randomMove === 'камень') {
-		msg.channel.send(`${msg.author}! Я победил. У меня был ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. А я вот победил, потому что у меня был **${randomMove}**`);
 		return;
 	}
 	else if (userMove === 'бумага' && randomMove === 'камень') {
-		msg.channel.send(`${msg.author}! Ты победил. У меня был ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. Ты победил, потому что у меня был **${randomMove}**`);
 		return;
 	}
 	else if (userMove === 'бумага' && randomMove === 'ножницы') {
-		msg.channel.send(`${msg.author}! Я победил. У меня были ${randomMove}`);
+		msg.channel.send(`Эй, ${msg.author}. А я вот победил, потому что у меня были **${randomMove}**`);
 		return;
 	}
 	else {

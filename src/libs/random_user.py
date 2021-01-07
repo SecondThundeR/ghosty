4a8785c500e9e7d1d614eb6b6b9de381ace721af
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 22:50:43 +0300

    Indent fix

--- src/libs/random_user.py
+++ src/libs/random_user.py
@@ -1,12 +1,11 @@
 """Library for obtaining information about a randomly selected user
 
-	This script allows to select a random user from list of server users
-	and get his information
+    This script allows to select a random user from list of server users
+    and get his information
 
-	This file can also be imported as a module and contains the following functions:
-		* get_random_user - return info of randomly chosen user
+    This file can also be imported as a module and contains the following functions:
+        * get_random_user - return info of randomly chosen user
 """
-
 
 import random
 from src.libs.database_handler import get_data_from_database
@@ -15,20 +14,20 @@ users = get_data_from_database('users', ['users_id'])
 
 
 async def get_random_user(msg, mode='default'):
-	"""Getting a random user from an list and returning user's info
+    """Getting a random user from an list and returning user's info
 
-	Parameters:
-		msg: Info about guild from message
-		mode (str): Mode of getting random user.
-		When 'shipping' is selected, executing different statement for getting
-		two random users. Defaults to 'default'
+    Parameters:
+        msg: Info about guild from message
+        mode (str): Mode of getting random user.
+        When 'shipping' is selected, executing different statement for getting
+        two random users. Defaults to 'default'
 
-	Returns:
-		User object about member which was randomly chosen. None for empty array of users
-	"""
-	if users is None:
-		return users
-	if mode == 'shipping':
-		return 'Not implemented'
-	member = await msg.guild.fetch_member(random.choice(users))
-	return member
+    Returns:
+        User object about member which was randomly chosen. None for empty array of users
+    """
+    if users is None:
+        return users
+    if mode == 'shipping':
+        return 'Not implemented'
+    member = await msg.guild.fetch_member(random.choice(users))
+    return member

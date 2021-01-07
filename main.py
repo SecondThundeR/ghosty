a95bcc46b3bd3eb81a742185a95204a16b7e6d75
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 14:58:02 +0300

    Update main script

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import discord
-from src.libs import database_handler
-from src.commands import random_word, me_message, get_help, random_number
+from src.libs.database_handler import *
+from src.commands import get_help, me_message, random_number, random_word
 
-TOKEN = database_handler.get_data_from_database('tokens', 'bot_token')
-SELECTED_BOT = database_handler.get_data_from_database('variables', 'currentSelectedBot')[0]
+
+TOKEN = get_data_from_database('tokens', 'bot_token')
+SELECTED_BOT = get_data_from_database('variables', 'currentSelectedBot')[0]
 ACTIVITY_NAME = 'Helltaker'
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-	database_handler.clear_data_on_execution()
+	clear_data_on_execution()
 	for guild in client.guilds:
 		async for member in guild.fetch_members(limit=None):
 			if member.bot:
-				database_handler.add_data_to_database('bots', ['bots_id'], [str(member.id)])
+				table = 'bots'
+				key = 'bots_id'
 			else:
-				database_handler.add_data_to_database('users', ['users_id'], [str(member.id)])
+				table = 'users'
+				key = 'users_id'
+			add_data_to_database(table, key, str(member.id))
 	await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=ACTIVITY_NAME))
 	print(f'Successfully logged in as {client.user}!\nSet {ACTIVITY_NAME} as an activity')
 
@@ -24,9 +28,9 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
 	if member.bot:
-		database_handler.add_data_to_database('bots', ['bots_id'], [str(member.id)])
+		add_data_to_database('bots', ['bots_id'], [str(member.id)])
 	else:
-		database_handler.add_data_to_database('users', ['users_id'], [str(member.id)])
+		add_data_to_database('users', ['users_id'], [str(member.id)])
 
 
 @client.event
@@ -36,14 +40,14 @@ async def on_message(message):
 	args = message.content.split(' ')
 	command = args.pop(0).lower()
 	if command in ('ху', 'who'):
-			await random_word.get_random_word(message, args)
+		await random_word.get_random_word(message, args)
 	elif command == 'йа':
-			await me_message.send_me_message(message, args)
+		await me_message.send_me_message(message, args)
 	elif command == 'хелп':
-			await get_help.send_help_message(message)
+		await get_help.send_help_message(message)
 	elif command == 'рандом':
-			await random_number.get_random_number(message, args)
+		await random_number.get_random_number(message, args)
 	else:
-			return
+		return
 
 client.run(TOKEN[int(SELECTED_BOT)])

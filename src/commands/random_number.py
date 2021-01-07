eda5af84ff9252a166d9aede8032adb0a7a3f18a
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 22:45:56 +0300

    Misc changes of indent and line length

--- src/commands/random_number.py
+++ src/commands/random_number.py
@@ -5,40 +5,75 @@ DELAY_TIME = 10
 
 
 async def get_random_number(msg, args):
-	if len(args) == 0:
-		await msg.channel.send(f'{msg.author.mention}, к сожалению, я не получил аргументов для вывода рандома', delete_after=DELAY_TIME)
-		await asyncio.sleep(DELAY_TIME)
-		await msg.delete()
-	elif len(args) == 1:
-		try:
-			range_number = int(args[0])
-			if range_number < 1 or range_number == 1:
-				await msg.channel.send(f'{msg.author.mention}, я не могу выдать рандомное число от 1 до {range_number}', delete_after=DELAY_TIME)
-				await asyncio.sleep(DELAY_TIME)
-				await msg.delete()
-			else:
-				random_number = random.randint(1, range_number)
-				await msg.channel.send(f'{msg.author.mention}, ваше рандомное число от 1 до {range_number}: **{random_number}**')
-		except ValueError:
-			await msg.channel.send(f'{msg.author.mention}, ваш аргумент был введён неправильно', delete_after=DELAY_TIME)
-			await asyncio.sleep(DELAY_TIME)
-			await msg.delete()
-	elif len(args) == 2:
-		try:
-			first_number = int(args[0])
-			second_number = int(args[1])
-			if first_number > second_number or first_number == second_number:
-				await msg.channel.send(f'{msg.author.mention}, я не могу выдать рандомное число в диапазоне от {first_number} до {second_number}', delete_after=DELAY_TIME)
-				await asyncio.sleep(DELAY_TIME)
-				await msg.delete()
-			else:
-				random_number = random.randint(first_number, second_number)
-				await msg.channel.send(f'{msg.author.mention}, ваше рандомное число от {first_number} до {second_number}: **{random_number}**')
-		except ValueError:
-			await msg.channel.send(f'{msg.author.mention}, ваш аргумент или аргументы были введены неправильно', delete_after=DELAY_TIME)
-			await asyncio.sleep(DELAY_TIME)
-			await msg.delete()
-	else:
-		await msg.channel.send(f'{msg.author.mention}, к сожалению, я получил аргументов больше, чем нужно, а именно `{len(args) - 2}`', delete_after=DELAY_TIME)
-		await asyncio.sleep(DELAY_TIME)
-		await msg.delete()
+    if len(args) == 0:
+        await msg.channel.send(
+            f'{msg.author.mention}, к сожалению, '
+            f'я не получил аргументов для вывода рандома',
+            delete_after=DELAY_TIME
+        )
+        await asyncio.sleep(DELAY_TIME)
+        await msg.delete()
+    elif len(args) == 1:
+        try:
+            range_number = int(args[0])
+            if range_number < 1 or range_number == 1:
+                await msg.channel.send(
+                    f'{msg.author.mention}, '
+                    f'я не могу выдать рандомное число '
+                    f'от 1 до {range_number}',
+                    delete_after=DELAY_TIME
+                )
+                await asyncio.sleep(DELAY_TIME)
+                await msg.delete()
+            else:
+                random_number = random.randint(1, range_number)
+                await msg.channel.send(
+                    f'{msg.author.mention}, '
+                    f'ваше рандомное число '
+                    f'от 1 до {range_number}: **{random_number}**'
+                )
+        except ValueError:
+            await msg.channel.send(
+                f'{msg.author.mention}, '
+                f'ваш аргумент был введён неправильно',
+                delete_after=DELAY_TIME
+            )
+            await asyncio.sleep(DELAY_TIME)
+            await msg.delete()
+    elif len(args) == 2:
+        try:
+            first_number = int(args[0])
+            second_number = int(args[1])
+            if first_number > second_number or first_number == second_number:
+                await msg.channel.send(
+                    f'{msg.author.mention}, '
+                    f'я не могу выдать рандомное число '
+                    f'в диапазоне от {first_number} до {second_number}',
+                    delete_after=DELAY_TIME
+                )
+                await asyncio.sleep(DELAY_TIME)
+                await msg.delete()
+            else:
+                random_number = random.randint(first_number, second_number)
+                await msg.channel.send(f'{msg.author.mention}, '
+                                       f'ваше рандомное число '
+                                       f'от {first_number} до {second_number}: '
+                                       f'**{random_number}**'
+                                       )
+        except ValueError:
+            await msg.channel.send(
+                f'{msg.author.mention}, '
+                f'ваш аргумент или аргументы '
+                f'были введены неправильно',
+                delete_after=DELAY_TIME)
+            await asyncio.sleep(DELAY_TIME)
+            await msg.delete()
+    else:
+        await msg.channel.send(
+            f'{msg.author.mention}, к сожалению, '
+            f'я получил аргументов больше, чем нужно, '
+            f'а именно `{len(args) - 2}`',
+            delete_after=DELAY_TIME
+        )
+        await asyncio.sleep(DELAY_TIME)
+        await msg.delete()
--- src/commands/random_word.py
+++ src/commands/random_word.py
@@ -7,35 +7,60 @@ DELAY_TIME = 3
 
 
 async def get_random_word(msg, args):
-	if len(args) == 0:
-		current_user = msg.author.mention
-	elif args[0] == 'рандом':
-		r_user = await random_user.get_random_user(msg)
-		if r_user is None:
-			await msg.channel.send(f'{msg.author.mention}, похоже я не получил список пользователей и поэтому мне не кого упоминать. проверьте базу данных и попробуйте ещё раз')
-			return
-		current_user = r_user.mention
-	else:
-		current_user = args[0]
-	if len(WORDS_ARRAY) == 0:
-		await msg.channel.send(f'{msg.author.mention}, я пока не знаю никаких слов, однако вы можете добавить новые слова в мой словарь', delete_after=DELAY_TIME)
-		await asyncio.sleep(DELAY_TIME)
-		await msg.delete()
-	elif check_for_spam(msg):
-		await msg.channel.send(f'{msg.author.mention} куда спамиш?', delete_after=DELAY_TIME)
-		await asyncio.sleep(DELAY_TIME)
-		await msg.delete()
-	else:
-		await msg.channel.send(f'{current_user} {random.choice(WORDS_ARRAY)}')
+    if len(args) == 0:
+        current_user = msg.author.mention
+    elif args[0] == 'рандом':
+        r_user = await random_user.get_random_user(msg)
+        if r_user is None:
+            await msg.channel.send(
+                f'{msg.author.mention}, похоже я не получил список пользователей и '
+                f'поэтому мне не кого упоминать'
+            )
+            return
+        current_user = r_user.mention
+    else:
+        current_user = args[0]
+    if len(WORDS_ARRAY) == 0:
+        await msg.channel.send(
+            f'{msg.author.mention}, я пока не знаю никаких слов, '
+            f'однако вы можете добавить новые слова в мой словарь',
+            delete_after=DELAY_TIME
+        )
+        await asyncio.sleep(DELAY_TIME)
+        await msg.delete()
+    elif check_for_spam(msg):
+        await msg.channel.send(
+            f'{msg.author.mention} куда спамиш?',
+            delete_after=DELAY_TIME
+        )
+        await asyncio.sleep(DELAY_TIME)
+        await msg.delete()
+    else:
+        await msg.channel.send(f'{current_user} {random.choice(WORDS_ARRAY)}')
 
 
 def check_for_spam(msg):
-	current_status = database_handler.get_data_from_database('variables', ['spammerID', 'spammerCount'])
-	if current_status[0] == msg.author.id:
-		if current_status[1] >= 3:
-			database_handler.edit_data_in_database('variables', ['spammerCount'], [0])
-			return True
-		database_handler.edit_data_in_database('variables', ['spammerCount'], [current_status[1] + 1])
-		return False
-	database_handler.edit_data_in_database('variables', ['spammerID', 'spammerCount'], [msg.author.id, 1])
-	return False
+    current_status = database_handler.get_data_from_database(
+        'variables',
+        ['spammerID', 'spammerCount']
+    )
+    if current_status[0] == msg.author.id:
+        if current_status[1] >= 3:
+            database_handler.edit_data_in_database(
+                'variables',
+                'spammerCount',
+                0
+            )
+            return True
+        database_handler.edit_data_in_database(
+            'variables',
+            'spammerCount',
+            current_status[1] + 1
+        )
+        return False
+    database_handler.edit_data_in_database(
+        'variables',
+        ['spammerID', 'spammerCount'],
+        [msg.author.id, 1]
+    )
+    return False

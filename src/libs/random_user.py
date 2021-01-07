edae33a307a0f403d58f728ab4f9711149c4cd15
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 15:03:10 +0300

    Update random_user and add docstring

--- src/libs/random_user.py
+++ src/libs/random_user.py
@@ -5,6 +5,18 @@ users = database_handler.get_data_from_database('users', ['users_id'])
 
 
 async def get_random_user(msg, mode='default'):
+	"""Get random user object from users id
+
+	Parameters:
+		msg: Info about guild from message
+		mode (str): Mode of getting random user. When 'shipping' is selected, executing different statement
+		for getting two random users. Defaults to 'default'
+
+	Returns:
+		Info about member which was randomly chosen. None for empty array of users
+	"""
+	if users is None:
+		return users
 	if mode == 'shipping':
 		return 'Not implemented'
 	member = await msg.guild.fetch_member(random.choice(users))

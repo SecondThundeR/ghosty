a60a6bbc3e3f342a61217f1329d4227d3f29379e
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 15:28:45 +0300

    Update main script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+"""secondthunder-py-bot
+
+    This is a Discord bot script that runs on the Discord.py library and
+    allows you to execute functions using specific text commands
+
+    This is the starting point of the bot control, from where it starts its
+    work. Here you can change the entire logic of the bot and adjust it to
+    fit your needs
+"""
+
 import discord
-from src.libs.database_handler import clear_data_on_execution, get_data_from_database, add_data_to_database
+from src.libs.database_handler import clear_data_on_execution
+from src.libs.database_handler import get_data_from_database
+from src.libs.database_handler import add_data_to_database
 from src.commands.get_help import send_help_message
 from src.commands.me_message import send_me_message
 from src.commands.random_number import get_random_number
@@ -14,43 +26,60 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	clear_data_on_execution()
-	for guild in client.guilds:
-		async for member in guild.fetch_members(limit=None):
-			if member.bot:
-				table = 'bots'
-				key = 'bots_id'
-			else:
-				table = 'users'
-				key = 'users_id'
-			add_data_to_database(table, key, str(member.id))
-	await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=ACTIVITY_NAME))
-	print(f'Successfully logged in as {client.user}!\nSet {ACTIVITY_NAME} as an activity')
+    """Execution of the necessary instructions during the bot launch and
+        displaying a message about successful launch
+    """
+    clear_data_on_execution()
+    for guild in client.guilds:
+        async for member in guild.fetch_members(limit=None):
+            if member.bot:
+                table = 'bots'
+                key = 'bots_id'
+            else:
+                table = 'users'
+                key = 'users_id'
+            add_data_to_database(table, key, str(member.id))
+    await client.change_presence(status=discord.Status.dnd,
+                                 activity=discord.Game(name=ACTIVITY_NAME))
+    print(f'Successfully logged in as {client.user}!\nSet {ACTIVITY_NAME} as an activity')
 
 
 @client.event
 async def on_member_join(member):
-	if member.bot:
-		add_data_to_database('bots', ['bots_id'], [str(member.id)])
-	else:
-		add_data_to_database('users', ['users_id'], [str(member.id)])
+    """Adding new server users to the database while the bot is running
+
+    Parameters:
+        member: Information about the user who joined the server
+    """
+    if member.bot:
+        add_data_to_database('bots', ['bots_id'], [member.id])
+    else:
+        add_data_to_database('users', ['users_id'], [member.id])
 
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	args = message.content.split(' ')
-	command = args.pop(0).lower()
-	if command in ('ху', 'who'):
-		await get_random_word(message, args)
-	elif command == 'йа':
-		await send_me_message(message, args)
-	elif command == 'хелп':
-		await send_help_message(message)
-	elif command == 'рандом':
-		await get_random_number(message, args)
-	else:
-		return
+    """Adding new server users to the database while the bot is running
+
+    **Noteworthy:** If a bot receives a message from another bot or from itself, or does
+    not receive the required command from the user, it does nothing
+
+    Parameters:
+        message: User message to perform required functions
+    """
+    if message.author == client.user:
+        return
+    args = message.content.split(' ')
+    command = args.pop(0).lower()
+    if command in ('ху', 'who'):
+        await get_random_word(message, args)
+    elif command == 'йа':
+        await send_me_message(message, args)
+    elif command == 'хелп':
+        await send_help_message(message)
+    elif command == 'рандом':
+        await get_random_number(message, args)
+    else:
+        return
 
 client.run(TOKEN[int(SELECTED_BOT)])

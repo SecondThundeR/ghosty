5045629566aafd81edbe2d5cc4fee15229093c6e
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-07 15:09:42 +0300

    Update random_user docstring

--- src/libs/random_user.py
+++ src/libs/random_user.py
@@ -1,11 +1,21 @@
-import random
-from src.libs import database_handler
+"""Library for obtaining information about a randomly selected user
 
-users = database_handler.get_data_from_database('users', ['users_id'])
+	This script allows to select a random user from list of server users
+	and get his information
+
+	This file can also be imported as a module and contains the following functions:
+		* get_random_user - return info of randomly chosen user
+"""
+
+
+import random
+from src.libs.database_handler import *
+
+users = get_data_from_database('users', ['users_id'])
 
 
 async def get_random_user(msg, mode='default'):
-	"""Get random user object from users id
+	"""Getting a random user from an list and returning user's info
 
 	Parameters:
 		msg: Info about guild from message
@@ -13,7 +23,7 @@ async def get_random_user(msg, mode='default'):
 		for getting two random users. Defaults to 'default'
 
 	Returns:
-		Info about member which was randomly chosen. None for empty array of users
+		User object about member which was randomly chosen. None for empty array of users
 	"""
 	if users is None:
 		return users

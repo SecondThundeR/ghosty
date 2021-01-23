964555fc395fa396068af5c803ec1458f4d1fcf7
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-23 22:17:47 +0300

    Update main script

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ fit your needs
 """
 
 
-import discord
+from discord import Client
+from discord import Status
+from discord import Game
+from discord import Intents
 from src.libs.database_handler import clear_data_on_execution
 from src.libs.database_handler import get_data_from_database
 from src.libs.database_handler import add_data_to_database
@@ -18,32 +21,32 @@ from src.commands.get_help import send_help_message
 from src.commands.me_message import send_me_message
 from src.commands.random_number import get_random_number
 from src.commands.random_word import get_random_word
+from src.commands.system_info import get_system_info
+from src.commands.user_checker import who_is_user
 
 
-TOKEN = get_data_from_database('tokens', 'bot_token')
+TOKENS = get_data_from_database('tokens', 'bot_token')
 SELECTED_BOT = get_data_from_database('variables', 'current_selected_bot')[0]
+ADMINS = get_data_from_database('admin_list', 'admins_id')
+BLOCKED = get_data_from_database('block_list', 'blocked_id')
 ACTIVITY_NAME = 'Helltaker'
-client = discord.Client()
+intents = Intents.default()
+intents.members = True
+client = Client(intents=intents)
 
 
 @client.event
 async def on_ready():
     """Execute necessary functions during the bot launch."""
-    print('Clearing database...')
     clear_data_on_execution()
-    print('Fetching all guild members...')
     for guild in client.guilds:
         async for member in guild.fetch_members(limit=None):
             if member.bot:
-                table = 'bots'
-                key = 'bots_id'
+                add_data_to_database('bots', 'bots_id', member.id)
             else:
-                table = 'users'
-                key = 'users_id'
-            add_data_to_database(table, key, str(member.id))
-    print(f'Setting \'{ACTIVITY_NAME}\' as an activity...')
-    await client.change_presence(status=discord.Status.dnd,
-                                 activity=discord.Game(name=ACTIVITY_NAME))
+                add_data_to_database('users', 'users_id', member.id)
+    await client.change_presence(status=Status.dnd,
+                                 activity=Game(name=ACTIVITY_NAME))
     print(f'Successfully logged in as {client.user}!')
 
 
@@ -52,12 +55,12 @@ async def on_member_join(member):
     """Add new server users to the database while the bot is running.
 
     Parameters:
-        member: Information about the user who joined the server
+        member (discord.member.Member): Information about the user who joined the server
     """
     if member.bot:
-        add_data_to_database('bots', 'bots_id', member.id)
+        add_data_to_database('members', 'bots_id', member.id)
     else:
-        add_data_to_database('users', 'users_id', member.id)
+        add_data_to_database('members', 'users_id', member.id)
 
 
 @client.event
@@ -70,21 +73,26 @@ async def on_message(message):
     Parameters:
         message (discord.message.Message): User message to perform required functions
     """
-    if message.author == client.user:
-        return
-    args = message.content.split(' ')
-    command = args.pop(0).lower()
-    if command in ('ху', 'who'):
-        await get_random_word(message, args)
-    elif command == 'йа':
-        await send_me_message(message, args)
-    elif command == 'хелп':
-        await send_help_message(message)
-    elif command == 'рандом':
-        await get_random_number(message, args)
-    elif command == 'макар':
-        await send_ded_makar_message(message, args)
-    else:
+    if message.author == client.user or message.author.id in BLOCKED:
         return
 
-client.run(TOKEN[int(SELECTED_BOT)])
+    if 'тест' in message.content or 'рандом' in message.content:
+        full_message = message.content.split(' ')
+        await who_is_user(message, full_message)
+    else:
+        args = message.content.split(' ')
+        command = args.pop(0).lower()
+        if command in ('ху', 'who'):
+            await get_random_word(message, args)
+        elif command == 'йа':
+            await send_me_message(message, args)
+        elif command == 'хелп':
+            await send_help_message(message)
+        elif command == 'рандом':
+            await get_random_number(message, args)
+        elif command == 'макар':
+            await send_ded_makar_message(message, args)
+        elif command == '.система' and message.author.id in ADMINS:
+            await get_system_info(message)
+
+client.run(TOKENS[int(SELECTED_BOT)])

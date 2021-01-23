98cbcff6d93fae680456a192bca876ff48d43899
SecondThundeR <awayfromgalaxy@gmail.com>
2021-01-24 00:01:50 +0300

    Add docs for new library

--- src/libs/words_base_handler.py
+++ src/libs/words_base_handler.py
@@ -1,4 +1,13 @@
-from requests import get
+"""Words base handler library.
+
+This script allows the user to work with a words base
+
+Currently this library able to restore developer's word base
+
+This file can also be imported as a module and contains the following functions:
+    * restore_dev_word_base - restores developer's word base
+"""
+import requests
 from src.libs.database_handler import add_data_to_database
 from src.libs.database_handler import remove_data_from_database
 from src.libs.file_handler import import_data_from_local_file
@@ -7,6 +16,14 @@ from src.libs.file_handler import delete_local_file
 
 
 def restore_dev_word_base(table, column, link, path):
+    """Download and restore latest dev's word base.
+
+    Parameters:
+        table (str): Name of table in DB
+        column (str): Name of column in table
+        link (str): Link to latest word base
+        path (str): Path to local file of word base
+    """
     _download_word_base_file(path, link)
     remove_data_from_database(table)
     words_array = import_data_from_local_file(path)
@@ -16,8 +33,13 @@ def restore_dev_word_base(table, column, link, path):
 
 
 def _download_word_base_file(path, link):
-    """Download dev's word base from Github."""
+    """Download dev's word base from Github.
+
+    Parameters:
+        path (str): Path to local file of word base
+        link (str): Link to latest word base
+    """
     if not check_if_local_file_exists(path):
-        r = get(link)
+        r = requests.get(link)
         with open(path, 'wb') as f:
             f.write(r.content)

21b2c90b59e7936d889f7bde6d832a425fc602ea
SecondThundeR <awayfromgalaxy@gmail.com>
2021-02-03 22:12:22 +0300

    Check for IndexError and return None

--- src/libs/user_handler.py
+++ src/libs/user_handler.py
@@ -25,17 +25,23 @@ async def get_random_user(msg, mode='default'):
     Returns:
         discord.member.Member: User object about member
             which was randomly chosen.
-            None for empty array of users
+        None for empty array of users
     """
     users = get_data_from_database(0, 'users', 'users_id')
     if mode == 'shipping':
-        first_member = await msg.guild.fetch_member(random.choice(users))
-        second_member = await msg.guild.fetch_member(random.choice(users))
-        while second_member == first_member:
+        try:
+            first_member = await msg.guild.fetch_member(random.choice(users))
             second_member = await msg.guild.fetch_member(random.choice(users))
-        return [first_member, second_member]
-    member = await msg.guild.fetch_member(random.choice(users))
-    return member
+            while second_member == first_member:
+                second_member = await msg.guild.fetch_member(random.choice(users))
+            return [first_member, second_member]
+        except IndexError:
+            return None
+    try:
+        member = await msg.guild.fetch_member(random.choice(users))
+        return member
+    except IndexError:
+        return None
 
 
 def get_members_name(member):

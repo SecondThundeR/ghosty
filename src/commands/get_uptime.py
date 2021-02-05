18cbeddc96e1e2acfa1d3ddaa29ecd9af0f9bcd6
SecondThundeR <awayfromgalaxy@gmail.com>
2021-02-05 18:04:37 +0300

    Remove debug print of td type

--- src/commands/get_uptime.py
+++ src/commands/get_uptime.py
@@ -45,7 +45,6 @@ def _format_timedelta(td):
     Returns:
         str: Formatted string
     """
-    print(type(td))
     minutes, seconds = divmod(td.seconds + td.days * 86400, 60)
     hours, minutes = divmod(minutes, 60)
     return '{:d}:{:02d}:{:02d}'.format(hours, minutes, seconds)

32c664cf1e0cc28dc8ae9c76317c36e44ffb50f1
SecondThundeR <awayfromgalaxy@gmail.com>
2021-03-08 20:44:51 +0300

    system: update
    - Refactor script name
    - Update variables
    - Update imports
    - Fix docstring

---
+++ src/cogs/system.py
@@ -0,0 +1,39 @@
+"""Get info about machine, where bot is running.
+
+This file can also be imported as a module and contains the following functions:
+    * get_system_info - retrieves system info and sends it as message
+"""
+
+
+from platform import system, release, version, processor, machine
+from asyncio import sleep
+
+
+DELAY_TIME = 5
+
+
+async def get_system_info(msg):
+    """Get system info and send it.
+
+    Parameters:
+        msg (discord.message.Message): Execute send to channel function
+    """
+    sys_name = system()
+    sys_release = release()
+    sys_version = version()
+    sys_cpu = processor()
+    sys_arch = machine()
+    if sys_cpu != '':
+        await msg.channel.send('Я работаю на '
+                               f'**{sys_name} {sys_release}** *({sys_version})*, '
+                               f'у которого процессор *({sys_cpu})* '
+                               f'имеет архитектуру - **{sys_arch}**',
+                               delete_after=DELAY_TIME)
+    else:
+        await msg.channel.send('Я работаю на '
+                               f'**{sys_name} {sys_release}** *({sys_version})*, '
+                               'у которого процессор имеет архитектуру - '
+                               f'**{sys_arch}**',
+                               delete_after=DELAY_TIME)
+    await sleep(DELAY_TIME)
+    await msg.delete()

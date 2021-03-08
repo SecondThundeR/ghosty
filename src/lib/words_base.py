80de3edea965bd2da0cc6c45eadedbc06d9609d5
SecondThundeR <awayfromgalaxy@gmail.com>
2021-03-08 20:09:37 +0300

    words_base: update
    - Update imports
    - Change some database function calls
    - Simplified functions names
    - Removed unnecessary functions

--- src/lib/words_base.py
+++ src/lib/words_base.py
@@ -1,90 +1,85 @@
-"""Words base handler library.
+"""Words Base Handler Library (Beta).
 
 This script allows the user to work with a words base (e.g. add new word,
 restore developers word base, etc.)
 
 This file can also be imported as a module and contains the following functions:
-    * add_word - adds word to main words base
+    * manage_words - adds/removes word to main words base
     * delete_word - removes word from main words base
-    * add_roulette_word - adds word to one of four roulette game words base
-    * delete_roulette_word - removes word from one of four roulette game words base
+    * manage_r_words - adds/removes word to one of four roulette game words base
     * restore_word_base - downloads and restores word base
 """
 
 
-import requests
-from src.libs.database_handler import add_data_to_database
-from src.libs.database_handler import remove_data_from_database
-from src.libs.database_handler import is_data_in_database
-from src.libs.files_handler import import_data_from_local_file
-from src.libs.files_handler import delete_local_file
+from requests import get
+from src.lib.database import get_data, modify_data
+from src.lib.files import import_data
 
 
-def add_word(word):
-    """Add word to main words base.
+def manage_words(word, mode=None):
+    """Manage main words base.
 
     Parameters:
-        word (str): Word to add to database
+        word (str): Word to add or remove
+        mode (str): Mode of action (addition/deletion)
 
     Returns:
-        str: Message about successful word addition
+        str: Function completion message
     """
-    add_data_to_database(2, 'main_words_base', 'words', word)
-    warn_msg = f'Хей, я успешно добавил слово "{word}" себе в базу!'
+    if mode == 'add':
+        modify_data(2, 'INSERT INTO main_words_base VALUES (?)', word)
+        warn_msg = f'Хей, я успешно добавил слово "{word}" себе в базу!'
+    elif mode == 'del':
+        if get_data(
+            2,
+            True,
+            'SELECT * FROM main_words_base WHERE words = ?',
+            word
+        ):
+            modify_data(2, 'DELETE FROM main_words_base WHERE words = ?', word)
+            warn_msg = f'Хей, я успешно удалил слово "{word}" из своей базы!'
+        else:
+            warn_msg = 'Ой, я не смог найти это слово. ' \
+                       'Вы уверены, что вы правильно его написали?'
+    else:
+        warn_msg = 'Похоже что-то пошло не так, ' \
+                   'свяжитесь с разработчиком для устранения проблемы'
     return warn_msg
 
 
-def delete_word(word):
-    """Remove word from main words base.
+def manage_r_word(word, base, mode=None):
+    """Manage russian roulette word base.
 
     Parameters:
-        word (str): Word to delete from database
-
-    Returns:
-        str: Message about successful word removal (Or not, if otherwise)
-    """
-    warn_msg = 'Ой, я не смог найти это слово. ' \
-               'Вы уверены, что вы правильно его написали?'
-    if is_data_in_database(2, 'main_words_base', 'words', word):
-        remove_data_from_database(2, 'main_words_base', 'words', word)
-        warn_msg = f'Хей, я успешно удалил слово "{word}" из своей базы!'
-    return warn_msg
-
-
-def add_roulette_word(word, base):
-    """Add word to roulette game words base.
-
-    Parameters:
-        word (str): Word to add to database
+        word (str): Word to add or remove
         base (str): Roulette's table to modify
+        mode (str): Mode of action (addition/deletion)
 
     Returns:
-        str: Message about successful word addition
+        str: Function completion message
     """
-    add_data_to_database(2, f'roulette_{base}_words', 'words', word)
-    warn_msg = f'Хей, я успешно добавил слово "{word}" себе в базу!'
+    if mode == 'add':
+        modify_data(2, f'INSERT INTO roulette_{base}_words VALUES (?)', word)
+        warn_msg = f'Хей, я успешно добавил слово "{word}" себе в базу!'
+    elif mode == 'del':
+        if get_data(
+            2,
+            True,
+            f'SELECT * FROM roulette_{base}_words WHERE words = ?',
+            word
+        ):
+            modify_data(2, f'DELETE FROM roulette_{base}_words WHERE words = ?', word)
+            warn_msg = f'Хей, я успешно удалил слово "{word}" из своей базы!'
+        else:
+            warn_msg = 'Ой, я не смог найти это слово. ' \
+                       'Вы уверены, что вы правильно его написали?'
+    else:
+        warn_msg = 'Похоже что-то пошло не так, ' \
+                   'свяжитесь с разработчиком для устранения проблемы'
     return warn_msg
 
 
-def delete_roulette_word(word, base):
-    """Remove word from roulette game words base.
-
-    Parameters:
-        word (str): Word to delete from database
-        base (str): Roulette's table to modify
-
-    Returns:
-        str: Message about successful word removal (Or not, if otherwise)
-    """
-    warn_msg = 'Ой, я не смог найти это слово. ' \
-               'Вы уверены, что вы правильно его написали?'
-    if is_data_in_database(2, f'roulette_{base}_words', 'words', word):
-        remove_data_from_database(2, f'roulette_{base}_words', 'words', word)
-        warn_msg = f'Хей, я успешно удалил слово "{word}" из своей базы!'
-    return warn_msg
-
-
-def restore_word_base(db_path, table, column, path, link):
+def restore_word_base(db_path, table, path, link):
     """Download and restore word base with link.
 
     This function allows to download .txt file and import it to database
@@ -92,24 +87,25 @@ def restore_word_base(db_path, table, column, path, link):
     Parameters:
         db_path (int): Path of database to edit data
         table (str): Name of table in DB
-        column (str): Name of column in table
         path (str): Path to local file of word base
         link (str): Link to latest word base
 
     Returns:
         bool: True if word base was imported successfully, False otherwise
     """
-    if not _download_word_base_file(path, link):
+    if not _download_wb_file(path, link):
         return False
-    remove_data_from_database(db_path, table)
-    words_array = import_data_from_local_file(path)
+    words_array = import_data(path)
     for element in words_array:
-        add_data_to_database(db_path, table, column, element)
-    delete_local_file(path)
+        modify_data(
+            db_path,
+            f'INSERT INTO {table} VALUES (?)',
+            element
+        )
     return True
 
 
-def _download_word_base_file(path, link):
+def _download_wb_file(path, link):
     """Download word base by link.
 
     This function writes downloaded data into .txt file
@@ -119,11 +115,11 @@ def _download_word_base_file(path, link):
         link (str): Link to latest word base
 
     Returns:
-        bool: True status code was 200, False - 404
+        bool: True if status code was 200, False if met other status codes
     """
-    r = requests.get(link)
-    if r.status_code == 404:
-        return False
-    with open(path, 'wb') as f:
-        f.write(r.content)
-    return True
+    r = get(link)
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            f.write(r.content)
+        return True
+    return False

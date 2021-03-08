acac18711f039fba6ee83ce4a9acb896f0d2528a
SecondThundeR <awayfromgalaxy@gmail.com>
2021-03-08 20:46:50 +0300

    manage_ignored: update
    - Fix docstrings
    - Update imports
    - Update variables

--- src/cogs/manage_ignored.py
+++ src/cogs/manage_ignored.py
@@ -1,4 +1,4 @@
-"""Script for managing ignore list of bot.
+"""Manage ignore list of bot.
 
 This script handles addition/removal id's of user to/from ignore list
 
@@ -8,10 +8,8 @@ This file can also be imported as a module and contains the following functions:
 
 
 from discord import channel
-from src.libs.database_handler import is_data_in_database
-from src.libs.database_handler import add_data_to_database
-from src.libs.database_handler import remove_data_from_database
-from src.libs.user_handler import is_user_admin
+from src.lib.database import modify_data
+from src.lib.users import is_user_admin, is_user_blocked
 
 
 async def ignored_manager(msg, args):
@@ -25,43 +23,43 @@ async def ignored_manager(msg, args):
         args (list): List with operation and user's ID
     """
     if (isinstance(msg.channel, channel.DMChannel)
-            and len(args) == 2 and is_user_admin(msg)):
+            and len(args) == 2 and is_user_admin(msg.author.id)):
         if args[0] == 'заблокировать':
             await _add_ignored(msg, args[1])
         elif args[0] == 'разблокировать':
             await _remove_ignored(msg, args[1])
 
 
-async def _add_ignored(msg, block_id):
-    """Add user's ID to ignore list.
+async def _add_ignored(msg, user_id):
+    """Add user's ID to blacklist.
 
     This function handles addition of user's ID to ignore list
 
     Parameters:
         msg (discord.message.Message): Execute send to channel function
-        block_id (str): ID of user to block
+        user_id (int): User's ID to ban
     """
-    if is_data_in_database(0, 'admin_list', 'admins_id', block_id):
+    if is_user_admin(user_id):
         await msg.channel.send('Я не могу заблокировать админа...')
     else:
-        if is_data_in_database(0, 'block_list', 'blocked_id', block_id):
+        if is_user_blocked(user_id):
             await msg.channel.send('Данный пользователь уже заблокирован')
         else:
-            add_data_to_database(0, 'block_list', 'blocked_id', block_id)
+            modify_data(0, 'INSERT INTO block_list VALUES (?)', user_id)
             await msg.channel.send('Я успешно заблокировал этого юзера')
 
 
-async def _remove_ignored(msg, block_id):
+async def _remove_ignored(msg, user_id):
     """Remove user's ID from ignore list.
 
     This function handles removal of user's ID from ignore list
 
     Parameters:
         msg (discord.message.Message): Execute send to channel function
-        block_id (str): ID of user to unblock
+        user_id (int): User's ID to unban
     """
-    if not is_data_in_database(0, 'block_list', 'blocked_id', block_id):
+    if not is_user_blocked(user_id):
         await msg.channel.send('Данный юзер уже разблокирован')
     else:
-        remove_data_from_database(0, 'block_list', 'blocked_id', block_id)
+        modify_data(0, 'DELETE FROM block_list WHERE blocked_id = ?', user_id)
         await msg.channel.send('Я успешно разблокировал этого юзера')

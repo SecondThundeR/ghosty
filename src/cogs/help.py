f774bab7d949ad364f96ec37061a522f5fc4b25e
SecondThundeR <awayfromgalaxy@gmail.com>
2021-03-08 20:33:06 +0300

    help: update
    - Refactor function name
    - Update imports
    - Change variable name
    - Fix docstring

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -1,4 +1,4 @@
-"""Script for getting help on using the bot.
+"""Send help message for using the bot.
 
 Provides a message with all available commands of bot
 
@@ -8,7 +8,7 @@ This file can also be imported as a module and contains the following functions:
 
 
 from discord import channel
-import asyncio
+from asyncio import sleep
 
 
 HELP_MESSAGE = 'Доступные команды бота: ' \
@@ -38,8 +38,8 @@ async def send_help_message(msg):
         await msg.channel.send(HELP_MESSAGE)
     else:
         await msg.author.send(HELP_MESSAGE)
-        warn_message = await msg.channel.send(f'{msg.author.mention}, проверь личку! '
-                                              'Я отправил тебе помощь по командам',
-                                              delete_after=DELAY_TIME)
-        await asyncio.sleep(DELAY_TIME)
-        await warn_message.delete()
+        warn_msg = await msg.channel.send(f'{msg.author.mention}, проверь личку! '
+                                          'Я отправил тебе помощь по командам',
+                                          delete_after=DELAY_TIME)
+        await sleep(DELAY_TIME)
+        await warn_msg.delete()

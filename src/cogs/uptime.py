e2a951fce49aa19a4681aa92c682f0724dd0e10c
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-24 23:43:44 +0300

    uptime: add timedelta formatting from utils

--- src/cogs/uptime.py
+++ src/cogs/uptime.py
@@ -11,6 +11,7 @@ from time import time as curr_time
 from asyncio import sleep
 from datetime import timedelta
 from src.lib.database import get_data
+from src.utils.timedelta_formatting import format_timedelta
 
 
 DELAY_TIME = 5
@@ -27,7 +28,7 @@ async def get_bot_uptime(msg):
         True,
         'SELECT bot_uptime FROM variables',
     )
-    time_string = _format_timedelta(timedelta(seconds=curr_uptime))
+    time_string = format_timedelta(timedelta(seconds=curr_uptime))
     if curr_uptime < 36000:
         await msg.channel.send(f'Я не сплю уже на протяжении **0{time_string}**',
                                delete_after=DELAY_TIME)
@@ -36,17 +37,3 @@ async def get_bot_uptime(msg):
                                delete_after=DELAY_TIME)
     await sleep(DELAY_TIME)
     await msg.delete()
-
-
-def _format_timedelta(td):
-    """Format timedelta to hours:minutes:seconds.
-
-    Parameters:
-        td (datetime.timedelta): Uptime of bot in timedelta
-
-    Returns:
-        str: Formatted string
-    """
-    minutes, seconds = divmod(td.seconds + td.days * 86400, 60)
-    hours, minutes = divmod(minutes, 60)
-    return '{:d}:{:02d}:{:02d}'.format(hours, minutes, seconds)

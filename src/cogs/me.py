c6f773113de05798c0cf97eecd62398981f119d1
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 01:46:13 +0300

    me: add tts for regular mode

--- src/cogs/me.py
+++ src/cogs/me.py
@@ -25,6 +25,10 @@ async def send_me_message(msg, args):
             args.pop(0)
             text_string = " ".join(args)
             await msg.channel.send(text_string, tts=True)
+        elif args[0] == 'ттс':
+            args.pop(0)
+            text_string = " ".join(args)
+            await msg.channel.send(f'{msg.author.mention} {text_string}', tts=True)
         else:
             text_string = " ".join(args)
             await msg.channel.send(f'{msg.author.mention} {text_string}')

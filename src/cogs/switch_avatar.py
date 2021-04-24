5655daf03f5a0147c010bae1946e23228afd7aa3
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 00:16:03 +0300

    switch_avatar: remove its own check for cooldown

--- src/cogs/switch_avatar.py
+++ src/cogs/switch_avatar.py
@@ -1,13 +1,10 @@
 import asyncio
-from time import time as curr_time
 from datetime import timedelta
-from src.lib.database import get_data, modify_data
 from src.utils.timedelta_formatting import format_timedelta
-from src.utils.avatar_changer import change_profile_picture
+from src.utils.avatar_changer import get_avatar_bytes
 
 
 DELAY_TIME = 5
-CHANGE_COOLDOWN = 1800
 
 
 async def switch_avatar(msg, client):
@@ -18,25 +15,16 @@ async def switch_avatar(msg, client):
         client (discord.Client): Execute avatar change function
     
     """
-    curr_cooldown = int(curr_time()) - get_data(
-        0,
-        True,
-        'SELECT avatar_cooldown FROM variables',
-    )
-    # Testing cooldown at 30 minutes for now
-    # If Discord API will "ban" avatar changing
-    # we need just simply raise the value
-    cooldown_diff = CHANGE_COOLDOWN - curr_cooldown
-    time_string = format_timedelta(timedelta(seconds=cooldown_diff))
-    if curr_cooldown < CHANGE_COOLDOWN:
+    avatar_data = get_avatar_bytes()
+    if isinstance(avatar_data, int):
+        time_string = format_timedelta(timedelta(seconds=avatar_data))
         await msg.channel.send('Пока что нельзя сменить аватарку. '
                                f'Попробуйте через **{time_string}**',
                                delete_after=DELAY_TIME)
         await asyncio.sleep(DELAY_TIME)
         await msg.delete()
     else:
-        modify_data(0, 'UPDATE variables SET avatar_cooldown = ?', int(curr_time()))
-        await client.user.edit(avatar=change_profile_picture())
+        await client.user.edit(avatar=avatar_data)
         await msg.channel.send('Аватарка успешно изменена!',
                                delete_after=DELAY_TIME)
         await asyncio.sleep(DELAY_TIME)

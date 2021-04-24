f59ac4c15d1dc6015ac720fd0c76a0131aba031a
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 02:05:01 +0300

    system: fix whitespace and under-indent

--- src/cogs/system.py
+++ src/cogs/system.py
@@ -35,12 +35,11 @@ async def get_system_info(msg, args):
                                        delete_after=DELAY_TIME)
             else:
                 await msg.channel.send('Я работаю на '
-                                      f'**{sys_name} {sys_release}** '
-                                      f'*({sys_version})*, '
-                                      'у которого процессор имеет архитектуру - '
-                                      f'**{sys_arch}**',
-                                      delete_after=DELAY_TIME)
-            
+                                       f'**{sys_name} {sys_release}** '
+                                       f'*({sys_version})*, '
+                                       'у которого процессор имеет архитектуру - '
+                                       f'**{sys_arch}**',
+                                       delete_after=DELAY_TIME)      
     else:
         await msg.channel.send('Я работаю на '
                                f'**{sys_name} {sys_release}** *({sys_version})*',

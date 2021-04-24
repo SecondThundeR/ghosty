2bab6232751936247ffb1654207b0c1d5d306a37
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 01:41:30 +0300

    system: add filter for getting full info

--- src/cogs/system.py
+++ src/cogs/system.py
@@ -12,28 +12,38 @@ from asyncio import sleep
 DELAY_TIME = 5
 
 
-async def get_system_info(msg):
+async def get_system_info(msg, args):
     """Get system info and send it.
 
     Parameters:
         msg (discord.message.Message): Execute send to channel function
+        args (list): Arguments for printing full info or reduced
     """
     sys_name = system()
     sys_release = release()
     sys_version = version()
     sys_cpu = processor()
     sys_arch = machine()
-    if sys_cpu != '':
-        await msg.channel.send('Я работаю на '
-                               f'**{sys_name} {sys_release}** *({sys_version})*, '
-                               f'у которого процессор *({sys_cpu})* '
-                               f'имеет архитектуру - **{sys_arch}**',
-                               delete_after=DELAY_TIME)
+    if len(args) > 0:
+        if args[0] == 'фулл':
+            if sys_cpu != '':
+                await msg.channel.send('Я работаю на '
+                                       f'**{sys_name} {sys_release}** '
+                                       f'*({sys_version})*, '
+                                       f'у которого процессор *({sys_cpu})* '
+                                       f'имеет архитектуру - **{sys_arch}**',
+                                       delete_after=DELAY_TIME)
+            else:
+                await msg.channel.send('Я работаю на '
+                                      f'**{sys_name} {sys_release}** '
+                                      f'*({sys_version})*, '
+                                      'у которого процессор имеет архитектуру - '
+                                      f'**{sys_arch}**',
+                                      delete_after=DELAY_TIME)
+            
     else:
         await msg.channel.send('Я работаю на '
-                               f'**{sys_name} {sys_release}** *({sys_version})*, '
-                               'у которого процессор имеет архитектуру - '
-                               f'**{sys_arch}**',
+                               f'**{sys_name} {sys_release}** *({sys_version})*',
                                delete_after=DELAY_TIME)
     await sleep(DELAY_TIME)
     await msg.delete()

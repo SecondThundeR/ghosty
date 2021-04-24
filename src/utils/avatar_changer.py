2cbc36ff8ce28414c33e0f613c30aabd69ac9564
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 00:13:12 +0300

    avatar_changer: add cooldown check

--- src/utils/avatar_changer.py
+++ src/utils/avatar_changer.py
@@ -1,18 +1,32 @@
-import random
-import pathlib
+from random import randrange
+from time import time as curr_time
+from pathlib import Path
+from src.lib.database import get_data, modify_data
 
 
-def change_profile_picture():
-    """Change profile picture of bot to random one.
+CHANGE_COOLDOWN = 900
+
+def get_avatar_bytes():
+    """Get bytes from avatar picture.
     
-    This function gets random number from 1 to 16 and selects avatar,
-    based on this number
+    This function has built-in check for
+    avatar change cooldown
 
     Returns:
-        bytes: Bytes of PNG picture
+        int: Cooldown time
+        bytes: Bytes of PNG
     """
-    avatar_path = f"{pathlib.Path().absolute()}/src/avatars/Avatar_{random.randrange(1, 16)}.png"
-    f = open(avatar_path, 'rb')
-    bytes = f.read()
-    f.close()
-    return bytes
+    curr_cooldown =  - get_data(
+        0,
+        True,
+        'SELECT avatar_cooldown FROM variables',
+    ) - int(curr_time())
+    if curr_cooldown > 0:
+        return int(curr_cooldown)
+    else:
+        modify_data(0, 'UPDATE variables SET avatar_cooldown = ?', int(curr_time()) + CHANGE_COOLDOWN)
+        avatar_path = f"{Path().absolute()}/src/avatars/Avatar_{randrange(1, 16)}.png"
+        f = open(avatar_path, 'rb')
+        bytes = f.read()
+        f.close()
+        return bytes

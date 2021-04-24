41a414a5da88596166da387fe63e6c73df288773
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-25 00:39:07 +0300

    avatar_changer: remove unnecessary else

--- src/utils/avatar_changer.py
+++ src/utils/avatar_changer.py
@@ -24,14 +24,13 @@ def get_avatar_bytes():
     ) - int(curr_time())
     if curr_cooldown > 0:
         return int(curr_cooldown)
-    else:
-        modify_data(
-            0, 
-            'UPDATE variables SET avatar_cooldown = ?', 
-            int(curr_time()) + CHANGE_COOLDOWN
-        )
-        avatar_path = f"{Path().absolute()}/src/avatars/Avatar_{randrange(1, 16)}.png"
-        with open(avatar_path, 'rb') as f:
-            bytes = f.read()
-        f.close()
-        return bytes
+    modify_data(
+        0, 
+        'UPDATE variables SET avatar_cooldown = ?', 
+        int(curr_time()) + CHANGE_COOLDOWN
+    )
+    avatar_path = f"{Path().absolute()}/src/avatars/Avatar_{randrange(1, 16)}.png"
+    with open(avatar_path, 'rb') as f:
+        bytes = f.read()
+    f.close()
+    return bytes

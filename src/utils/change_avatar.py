47f05ec57bb68fac19f1b83e1c8af0f3b8070ede
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-24 23:19:35 +0300

    change_avatar: add initial docstrings

--- src/utils/change_avatar.py
+++ src/utils/change_avatar.py
@@ -2,6 +2,14 @@ import random
 import pathlib
 
 def change_profile_picture():
+    """Change profile picture of bot to random one.
+    
+    This function gets random number from 1 to 16 and selects avatar,
+    based on this number
+
+    Returns:
+        bytes: Bytes of PNG picture
+    """
     avatar_path = f"{pathlib.Path().absolute()}/src/avatars/Avatar_{random.randrange(1, 16)}.png"
     f = open(avatar_path, 'rb')
     bytes = f.read()

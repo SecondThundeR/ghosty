8921df9737ceec12afc2ad091920602c01e0bdb4
SecondThundeR <awayfromgalaxy@gmail.com>
2021-04-29 09:17:56 +0300

    exceptions: add needed spacing and remove `pass`

--- src/lib/exceptions.py
+++ src/lib/exceptions.py
@@ -1,7 +1,6 @@
 class CustomErrors(Exception):
     """Base class for other exceptions"""
-    pass
+
 
 class UsersNotFound(CustomErrors):
     """Raised when users cannot be found in database"""
-    pass

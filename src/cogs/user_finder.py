4c0e926778de3e9216ce9ad045a3c6f2309652a7
SecondThundeR <awayfromgalaxy@gmail.com>
2021-05-05 00:51:06 +0300

    user_finder: switch to args and capitalize words

--- src/cogs/user_finder.py
+++ src/cogs/user_finder.py
@@ -16,16 +16,17 @@ class UserFinder(commands.Cog):
         self.delay_time = 2
 
     @commands.command(aliases=['поиск'])
-    async def user_finder_hub(self, ctx, word=None):
+    async def user_finder_hub(self, ctx, *, args):
         """Execute required finder mode depending on arguments.
 
         Parameters:
             ctx (commands.context.Context): Context object to execute functions
-            word (str | None): Word for user finder
+            args (tuple): Arguments (Words) for user finder
         """
-        if not word:
+        if not args:
             return
-        await UserFinder.user_finder_execute(self, ctx, word)
+        formatted_word = " ".join(word.capitalize() for word in args.split())
+        await UserFinder.user_finder_execute(self, ctx, formatted_word)
 
     async def user_finder_execute(self, ctx, word):
         """Get random user and launch 'finder' of *any word*.
@@ -46,7 +47,7 @@ class UserFinder(commands.Cog):
             await ctx.send('*пип*')
             await asyncio.sleep(self.delay_time)
             i += 1
-        await init_msg.reply(f'"{word.capitalize()}" найден. '
+        await init_msg.reply(f'"{word}" найден. '
                              f'Им оказался - {random_user.mention}')
 
 

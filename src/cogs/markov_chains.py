c92ca0f49899e8ac0ff69e878e1cdc2c6f971f1f
SecondThundeR <awayfromgalaxy@gmail.com>
2021-05-15 23:14:58 +0300

    markov_chains: initial addition

---
+++ src/cogs/markov_chains.py
@@ -0,0 +1,30 @@
+"""
+This code was taken from https://habr.com/ru/post/510798/
+Very glad to see such articles on Habr!
+"""
+
+
+import asyncio
+import src.utils.markov_utils as markov_utils
+from discord.ext import commands
+
+
+class MarkovCommand(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.delay_time = 5
+
+    @commands.command(aliases=['ген', 'марков'])
+    async def send_markov_sentence(self, ctx):
+        new_sentence = markov_utils.generate_new_sentence()
+        if not new_sentence:
+            await ctx.reply('Похоже, у меня недостаточно слов в базе данных :(',
+                            delete_after=self.delay_time)
+            await asyncio.sleep(self.delay_time)
+            await ctx.message.delete()
+        else:
+            await ctx.reply(new_sentence)
+
+
+def setup(client):
+    client.add_cog(MarkovCommand(client))

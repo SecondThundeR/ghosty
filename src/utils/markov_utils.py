24057308dde66163d7e3e9d635ab1076feda6424
SecondThundeR <awayfromgalaxy@gmail.com>
2021-05-16 16:04:03 +0300

    markov_chains: update
    - Remove check for correct word
    - Remove credits of code (as this code is duplicated on many sites)
    - Change names of some functions
    - Replace default range size for words generate
    - Add handler for custom amount of words
    - Add errors handler for message length
    - Add many checks and handlers to prevent a lot of errors in future

--- src/utils/markov_utils.py
+++ src/utils/markov_utils.py
@@ -4,26 +4,11 @@ import src.lib.database as database
 
 def message_words_to_db(words):
     for word in words:
-        checked_word = check_word(word)
-        if checked_word:
-            database.modify_data(
-                'wordsDB',
-                "INSERT INTO markov_words VALUES (?)",
-                checked_word
-            )
-
-
-def check_word(word):
-    if (word.startswith('<@')
-            or word.startswith('<:')
-            or word.startswith('.')
-            or word.startswith('!')
-            or word.startswith('-')):
-        return None
-    if '@everyone' in word or '@here' in word:
-        return None
-    return word
-
+        database.modify_data(
+            'wordsDB',
+            'INSERT INTO markov_words VALUES (?)',
+            word
+        )
 
 def markov_delay_handler(mode):
     current_delay = database.get_data(
@@ -53,11 +38,7 @@ def markov_delay_handler(mode):
         return [current_delay, msg_counter]
 
 
-def generate_new_sentence():
-    """
-    This code was taken from https://habr.com/ru/post/510798/
-    Very glad to see such articles on Habr!
-    """
+def prepare_markov_chains():
     word_dict = {}
     database_words = database.get_data(
         'wordsDB',
@@ -73,17 +54,51 @@ def generate_new_sentence():
             word_dict[word_1].append(word_2)
         else:
             word_dict[word_1] = [word_2]
-    first_word = random.choice(database_words)
-    while first_word.islower():
-        chain = [first_word]
-        n_words = random.randint(20, 80)
-        first_word = random.choice(database_words)
-
-        for _ in range(n_words):
-            chain.append(random.choice(word_dict[chain[-1]]))
-    return ' '.join(chain)
+    return [database_words, word_dict]
 
 
 def make_pairs(words):
     for i in range(len(words) - 1):
         yield (words[i], words[i + 1])
+
+
+def generate_sentence(data):
+    first_word = random.choice(data[0])
+    chain = [first_word]
+    if len(data) == 3:
+        n_words = data[2]
+    else:
+        n_words = random.randint(30, 80)
+    i = 1
+    fail_counter = 0
+    while i < n_words:
+        if len(' '.join(chain)) > 2000:
+            return chain[:len(chain) - 1]
+        try:
+            chain.append(random.choice(data[1][chain[-1]]))
+            i += 1
+        except KeyError:
+            random_chain_key = random.choice(chain)
+            while random_chain_key == chain[-1]:
+                fail_counter += 1
+                if fail_counter == 3:
+                    return None
+                random_chain_key = random.choice(chain)
+            chain.append(random.choice(data[1][random_chain_key]))
+            i += 1
+    return chain
+
+
+def return_checked_sentence(number):
+    data = prepare_markov_chains()
+    if isinstance(data, bool):
+        return data
+    if number and number.isnumeric():
+        data.append(int(number))
+    new_sentence = generate_sentence(data)
+    while not new_sentence:
+        if len(data) == 3:
+            data = data[:2]
+        new_sentence = generate_sentence(data)
+    joined_sentence = ' '.join(new_sentence)
+    return joined_sentence

770836a8f9f6aeff910cd0daf27928935281d81f
SecondThundeR <awayfromgalaxy@gmail.com>
2021-05-16 11:12:28 +0300

    markov_utils: update
    - Remove check for duplicate words
    - Add one more check for word checker
    - Slightly changed markov_delay random range
    - Revert generate_new_sentence to previous look

--- src/utils/markov_utils.py
+++ src/utils/markov_utils.py
@@ -3,16 +3,9 @@ import src.lib.database as database
 
 
 def message_words_to_db(words):
-    database_words = database.get_data(
-        'wordsDB',
-        False,
-        "SELECT * FROM markov_words"
-    )
-    if database_words is None:
-        database_words = [database_words]
     for word in words:
         checked_word = check_word(word)
-        if checked_word and checked_word not in database_words:
+        if checked_word:
             database.modify_data(
                 'wordsDB',
                 "INSERT INTO markov_words VALUES (?)",
@@ -21,7 +14,10 @@ def message_words_to_db(words):
 
 
 def check_word(word):
-    if word.startswith('<@') or word.startswith('<:') or word.startswith('.'):
+    if (word.startswith('<@')
+            or word.startswith('<:')
+            or word.startswith('.')
+            or word.startswith('!')):
         return None
     if '@everyone' in word or '@here' in word:
         return None
@@ -49,7 +45,7 @@ def markov_delay_handler(mode):
         database.modify_data(
             'mainDB',
             "UPDATE variables SET markov_delay = ?, msg_counter = ?",
-            random.randint(35, 75),
+            random.randint(20, 45),
             0
         )
     elif mode == 'get':
@@ -58,7 +54,6 @@ def markov_delay_handler(mode):
 
 def generate_new_sentence():
     word_dict = {}
-    chain = None
     database_words = database.get_data(
         'wordsDB',
         False,
@@ -80,14 +75,8 @@ def generate_new_sentence():
         first_word = random.choice(database_words)
 
         for _ in range(n_words):
-            try:
-                chain.append(random.choice(word_dict[chain[-1]]))
-            except KeyError:
-                chain = [random.choice(database_words)]
-    if not chain:
-        generate_new_sentence()
-    else:
-        return ' '.join(chain)
+            chain.append(random.choice(word_dict[chain[-1]]))
+    return ' '.join(chain)
 
 
 def make_pairs(words):

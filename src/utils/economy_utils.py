3179b29461a76b74c1bba8aa5d61b4beba8994ed
Владислав <awayfromgalaxy@gmail.com>
2021-08-15 02:39:43 +0300

    economy_utils: misc update
    - Added docstrings
    - Added check for sender balance to prevent going to negative balance while transferring

--- src/utils/economy_utils.py
+++ src/utils/economy_utils.py
@@ -1,10 +1,33 @@
+"""Utils for server's economy.
+
+This utils contains all needed functions to handle economy operations
+between users or certain commands.
+
+This file can also be imported as a module and contains the following functions:
+    * check_account - checks is a user points account exists
+    * add_new_account - adds new points account to DB
+    * delete_account - deletes active points acccount from DB
+    * get_account_balance - returns balance of a user account
+    * tranfer_points - transfers points between two points accounts
+"""
+
 import src.lib.database as database
 
 
+# Initial values for points accounts
+# Can be changed later on
 DEFAULT_BALANCE = 1000
 
 
 def check_account(user_id):
+    """Check if points account exists.
+
+    Args:
+        user_id (int): The ID of ther user
+
+    Returns:
+        bool: True if account exists, False otherwise.
+    """
     account_status = database.get_data(
         'pointsDB',
         True,
@@ -15,6 +38,14 @@ def check_account(user_id):
 
 
 def add_new_account(user_id):
+    """Add new points accounts to database.
+
+    Args:
+        user_id (int): The ID of the user
+
+    Returns:
+        bool: True if account was added, False if account is already added
+    """
     account_status = check_account(user_id)
     if account_status:
         return False
@@ -27,6 +58,14 @@ def add_new_account(user_id):
 
 
 def delete_account(user_id):
+    """Remove active points account from database.
+
+    Args:
+        user_id (int): The ID of the user
+
+    Returns:
+        bool: True if account was deleted, False if account was already deleted
+    """
     account_status = check_account(user_id)
     if not account_status:
         return False
@@ -39,6 +78,15 @@ def delete_account(user_id):
 
 
 def get_account_balance(user_id):
+    """Get balance of user points account.
+
+    Args:
+        user_id (int): The ID of the user
+
+    Returns:
+        int: The balance of the user account
+        None: If the user does not have an account
+    """
     account_status = check_account(user_id)
     if not account_status:
         return None
@@ -52,6 +100,16 @@ def get_account_balance(user_id):
 
 
 def transfer_points(sender_id, reciever_id, points):
+    """Manage transferring points balance between two points accounts.
+
+    Args:
+        sender_id (int): The ID of the sender
+        reciever_id (int): The ID of the reciever
+        points (int): The amount of points to transfer
+    """
+    sender_balance = get_account_balance(sender_id)
+    if sender_balance - points <= 0:
+        return False
     database.modify_data(
         'pointsDB',
         'UPDATE points SET points_balance = points_balance - ? WHERE user_id = ?',
@@ -62,3 +120,4 @@ def transfer_points(sender_id, reciever_id, points):
         'UPDATE points SET points_balance = points_balance + ? WHERE user_id = ?',
         points, reciever_id
     )
+    return True

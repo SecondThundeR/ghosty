6583f85db21ca1178b5debb35f208a66e5f5e843
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-26 18:06:10 +0300

    utils: initial commit
    stringUtils:
    - Contains exported reverseString function
    userUtils:
    - Contains expored fetching role/usernames by id functions and getting user mention function (parseRawID aren't exported as it used only inside fetch functions)

---
+++ src/utils/userUtils.js
@@ -0,0 +1,23 @@
+function fetchUsernameByID(currentGuild, rawUserID) {
+	const userID = parseRawID(rawUserID);
+	const guildUser = currentGuild.members.cache.get(userID);
+	return guildUser.displayName;
+}
+
+function fetchRolenameByID(currentGuild, rawRoleID) {
+	const roleID = parseRawID(rawRoleID);
+	const roleName = currentGuild.roles.cache.get(roleID).name;
+	return roleName;
+}
+
+function parseRawID(rawID) {
+	return rawID.slice(3, -1);
+}
+
+function getUserMention(userID) {
+	return `<@${userID}>`;
+}
+
+exports.fetchUsernameByID = fetchUsernameByID;
+exports.fetchRolenameByID = fetchRolenameByID;
+exports.getUserMention = getUserMention;

56b9b0ccc85a3750c56d075155095f3467b22837
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-26 18:03:11 +0300

    deploy-commands.js: update
    Switched from spaces to tabs
    Add check for process.argv to run guild or global commands update

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,32 +3,36 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const dotenv = require('dotenv');
 
-// Gets all ENV variables from .env file
 dotenv.config();
 
 const commands = [];
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./src/commands/${file}`);
-    commands.push(command.data.toJSON());
+	const command = require(`./src/commands/${file}`);
+	commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const args = process.argv.slice(2);
 
 (async () => {
-    try {
-        await rest.put(
-            // Line below is using global commands, instead of guild commands for dev purposes
-            // Routes.applicationCommands(process.env.CLIENT_ID),
-
-            // Line below is using guild commands to quickly check and test commands without waiting
-            // for updating global commands
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
-        );
-        console.log('Successfully registered application commands.');
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		if (args[0] === 'global') {
+			await rest.put(
+				Routes.applicationCommands(process.env.CLIENT_ID),
+			);
+			console.log('Successfully registered global application commands.');
+		}
+		else {
+			await rest.put(
+				Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+				{ body: commands },
+			);
+			console.log('Successfully registered guild application commands.');
+		}
+	}
+	catch (error) {
+		console.error(error);
+	}
 })();

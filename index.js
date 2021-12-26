81a879e582ea9334254d6786288a1a42c3d38432
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-26 18:01:44 +0300

    index.js: update
    Moved out 'ready' and 'interactionCreate' events to ./src/events folder

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./src/commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(process.env.TOKEN);

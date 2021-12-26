a046e0bb86d4f1e3e440952def4ea2629422f2bb
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-26 23:34:02 +0300

    ready: add MongoDB connection
    At the moment, the connection is disabled until future use

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,12 @@
+// const mongoose = require('mongoose');
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
+	async execute(client) {
+		// await mongoose.connect(process.env.MONGO_URI, {
+		// 	keepAlive: true,
+		// });
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };

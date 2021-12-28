f2f57cc3089f2898028d1c3d1dde6e4672c6a6a5
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-28 19:48:37 +0300

    me: update
    - Changed constants in `sendMessageFromBot`
    - Fix style (Applied formatting from ESLint)

--- src/commands/me.js
+++ src/commands/me.js
@@ -1,9 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getChannel } = require('../utils/guildUtils');
 const { getUserMention } = require('../utils/userUtils');
 
 function sendMessageFromBot(interaction, textToSend, textMode) {
-    const channel = interaction.client.channels.cache.get(interaction.channelId);
-    const regularMessage = `${getUserMention(interaction.user.id)} сказал: `;
+    const channel = getChannel(interaction);
+    const userID = interaction.user.id;
+    const regularMessage = `${getUserMention(userID)} сказал: `;
 
     switch (textMode) {
     case 'анонттс':
@@ -25,41 +27,51 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('йа')
         .setDescription('Аналог команды `/me`')
-        .addSubcommand(subcommand => subcommand
-            .setName('обычный')
-            .setDescription('Управляет отправкой сообщения в обычном режиме')
-            .addStringOption(option => option
-                .setName('текст')
-                .setDescription('Текст, который будет написан от лица бота')
-                .setRequired(true),
-            ),
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('обычный')
+                .setDescription('Управляет отправкой сообщения в обычном режиме')
+                .addStringOption((option) =>
+                    option
+                        .setName('текст')
+                        .setDescription('Текст, который будет написан от лица бота')
+                        .setRequired(true),
+                ),
         )
-        .addSubcommand(subcommand => subcommand
-            .setName('анон')
-            .setDescription('Управляет отправкой сообщения без упоминания автора')
-            .addStringOption(option => option
-                .setName('текст')
-                .setDescription('Текст, который будет написан от лица бота')
-                .setRequired(true),
-            ),
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('анон')
+                .setDescription('Управляет отправкой сообщения без упоминания автора')
+                .addStringOption((option) =>
+                    option
+                        .setName('текст')
+                        .setDescription('Текст, который будет написан от лица бота')
+                        .setRequired(true),
+                ),
         )
-        .addSubcommand(subcommand => subcommand
-            .setName('ттс')
-            .setDescription('Управляет отправкой сообщения в режиме TTS')
-            .addStringOption(option => option
-                .setName('текст')
-                .setDescription('Текст, который будет написан от лица бота')
-                .setRequired(true),
-            ),
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('ттс')
+                .setDescription('Управляет отправкой сообщения в режиме TTS')
+                .addStringOption((option) =>
+                    option
+                        .setName('текст')
+                        .setDescription('Текст, который будет написан от лица бота')
+                        .setRequired(true),
+                ),
         )
-        .addSubcommand(subcommand => subcommand
-            .setName('анонттс')
-            .setDescription('Управляет отправкой сообщения без упоминания автора в режиме TTS')
-            .addStringOption(option => option
-                .setName('текст')
-                .setDescription('Текст, который будет написан от лица бота')
-                .setRequired(true),
-            ),
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('анонттс')
+                .setDescription(
+                    'Управляет отправкой сообщения без упоминания автора в режиме TTS',
+                )
+                .addStringOption((option) =>
+                    option
+                        .setName('текст')
+                        .setDescription('Текст, который будет написан от лица бота')
+                        .setRequired(true),
+                ),
         ),
     async execute(interaction) {
         const sendMode = interaction.options.getSubcommand();

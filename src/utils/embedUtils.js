72ff41b5ba319333bccd3f05dcbec210b3ee31c2
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-29 12:03:14 +0300

    embedUtils: add more functions for creating embeds

--- src/utils/embedUtils.js
+++ src/utils/embedUtils.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+
 const logoColors = [
     '#C9CBFF',
     '#7868E6',
@@ -21,4 +23,24 @@ function getRandomEmbedColor() {
     return logoColors[Math.floor(Math.random() * logoColors.length)];
 }
 
-exports.getRandomEmbedColor = getRandomEmbedColor;
+function createPollEmbed(userName, pollText, pollTime) {
+    const pollEmbed = new MessageEmbed()
+        .setColor(getRandomEmbedColor())
+        .setTitle(`Новое голосование от ${userName}`)
+        .setDescription(pollText)
+        .addFields(
+            { name: 'Время для голосования', value: `${pollTime} секунд` },
+        );
+    return pollEmbed;
+}
+
+function createResultsEmbed(userName, pollText, pollResults) {
+    const resultsEmbed = new MessageEmbed()
+        .setColor(getRandomEmbedColor())
+        .setTitle(`Результаты голосования "${pollText}" от ${userName}`)
+        .setDescription(pollResults);
+    return resultsEmbed;
+}
+
+exports.createPollEmbed = createPollEmbed;
+exports.createResultsEmbed = createResultsEmbed;

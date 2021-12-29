677b253f51e3ea102f5fd767d0fbd11d460e5fd1
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-29 12:03:22 +0300

    pollUtils: initial commit

---
+++ src/utils/pollUtils.js
@@ -0,0 +1,33 @@
+async function addVoteReactions(message) {
+    try {
+        await message.react('👍');
+        await message.react('👎');
+    }
+    catch (error) {
+        console.log('Произошла ошибка при добавлении реакций на голосование:', error);
+    }
+}
+
+function parseCollected(collected) {
+    const reactionsMap = {
+        '👍': 0,
+        '👎': 0,
+    };
+    collected.map(
+        reaction => reactionsMap[reaction.emoji.name] = reaction.count - 1,
+    );
+    return reactionsMap;
+}
+
+function evaluateResults(pollData) {
+    const likesCount = pollData['👍'];
+    const dislikesCount = pollData['👎'];
+
+    if (likesCount > dislikesCount) return `Сокрушительная победа! - 👍: ${likesCount} / 👎: ${dislikesCount}`;
+    if (likesCount < dislikesCount) return `Безжалостное поражение! - 👍: ${likesCount} / 👎: ${dislikesCount}`;
+    return `Возможно, победа дружбы! - 👍: ${likesCount} / 👎: ${dislikesCount}`;
+}
+
+exports.addVoteReactions = addVoteReactions;
+exports.parseCollected = parseCollected;
+exports.evaluateResults = evaluateResults;

5a0f2f4661a55242f3e74b4984523536d671dedc
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-29 15:04:47 +0300

    timeUtils: add time formatter function

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -2,4 +2,11 @@ function formatTimeValue(s) {
     return (s < 10 ? '0' : '') + s;
 }
 
-exports.formatTimeValue = formatTimeValue;
+function getFormattedTime(time) {
+    const hours = formatTimeValue(Math.floor(time / (60 * 60)));
+    const minutes = formatTimeValue(Math.floor(time % (60 * 60) / 60));
+    const seconds = formatTimeValue(Math.floor(time % 60));
+    return `**${hours}:${minutes}:${seconds}**`;
+}
+
+exports.getFormattedTime = getFormattedTime;

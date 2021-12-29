140dd5344b6c95e5cf9d715ef3a5a3de151a559f
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-29 20:36:22 +0300

    userUtils: add `getRandomUser`

--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -1,3 +1,6 @@
+const botUsersModel = require('../schemas/botUsers');
+const { getModelData } = require('./databaseUtils');
+
 function fetchUsernameByID(currentGuild, rawUserID) {
     const userID = parseRawID(rawUserID);
     const guildUser = currentGuild.members.cache.get(userID);
@@ -14,5 +17,20 @@ function parseRawID(rawID) {
     return rawID.slice(3, -1);
 }
 
+async function getRandomUser(guild) {
+    let guildUsers;
+    const botUsersData = await getModelData(botUsersModel);
+    for (const item of botUsersData) {
+        if (item.guildID == guild.id) {
+            guildUsers = item.guildMembers;
+            break;
+        }
+    }
+    const randomUserID = guildUsers[Math.floor(Math.random() * guildUsers.length)];
+    const guildUser = guild.members.cache.get(randomUserID);
+    return guildUser;
+}
+
 exports.fetchUsernameByID = fetchUsernameByID;
 exports.fetchRolenameByID = fetchRolenameByID;
+exports.getRandomUser = getRandomUser;

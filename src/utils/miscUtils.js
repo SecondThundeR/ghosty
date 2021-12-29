fa2a7ec7c4f43c932d1ecb1096074fc2b9678022
SecondThundeR <awayfromgalaxy@gmail.com>
2021-12-29 20:35:53 +0300

    miscUtils: add `getRandomNumber`

--- src/utils/miscUtils.js
+++ src/utils/miscUtils.js
@@ -2,4 +2,11 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function getRandomNumber(lowNum, highNum) {
+    if (lowNum > highNum || (lowNum < 0 || highNum < 0)) return null;
+    return Math.floor(Math.random() * (highNum - lowNum)) + lowNum;
+}
+
+
 exports.delay = delay;
+exports.getRandomNumber = getRandomNumber;

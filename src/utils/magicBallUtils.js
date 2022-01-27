0db032b917f3e39d2251471138b0df292cbe5313
SecondThundeR <awayfromgalaxy@gmail.com>
2022-01-27 16:55:52 +0300

    magicBallUtils: initial commit

---
+++ src/utils/magicBallUtils.js
@@ -0,0 +1,30 @@
+const magicBallAnswers = [
+    'Бесспорно',
+    'Предрешено',
+    'Никаких сомнений',
+    'Определённо да',
+    'Можешь быть уверен в этом',
+    'Мне кажется — «да»',
+    'Вероятнее всего',
+    'Хорошие перспективы',
+    'Знаки говорят — «да»',
+    'Да',
+    'Пока не ясно, попробуй снова',
+    'Спроси позже',
+    'Лучше не рассказывать',
+    'Сейчас нельзя предсказать',
+    'Сконцентрируйся и спроси опять',
+    'Даже не думай',
+    'Мой ответ — «нет»',
+    'По моим данным — «нет»',
+    'Перспективы не очень хорошие',
+    'Весьма сомнительно',
+];
+
+function returnMagicBallAnswer(userQuestion) {
+    const magicBallAnswer = magicBallAnswers[Math.floor(Math.random() * magicBallAnswers.length)];
+    const answerMsg = `Вопрос: **${userQuestion}**\nОтвет: **${magicBallAnswer}**`;
+    return answerMsg;
+}
+
+exports.returnMagicBallAnswer = returnMagicBallAnswer;
